7e3d535400fc94349bf66dbb3292bbe61d540943
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-12-11 23:34:32 +0600

    layout improved

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router';
-import Navbar from '../pages/Navbar/Navbar';
-import Footer from '../pages/shared/Footer';
+import Footer from '../pages/shared/Footer/Footer';
+import Navbar from '../pages/shared/Navbar/Navbar';
 
 const RootLayout = () => {
     return (
