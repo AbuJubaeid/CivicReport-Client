fde6b31d9df44b771d3d781cd5162cc49266baea
AbuJubaeid <abujubaeidpranto2@gmail.com>
2026-02-07 12:30:54 +0600

    implemented search function

--- src/pages/DashBoard/UserManager/UserManager.jsx
+++ src/pages/DashBoard/UserManager/UserManager.jsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { FaUserShield } from "react-icons/fa";
 import { GiShieldDisabled } from "react-icons/gi";
 import Swal from "sweetalert2";
@@ -6,81 +7,99 @@ import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const UserManager = () => {
   const axiosSecure = useAxiosSecure();
-  const { refetch,  data: users = [] } = useQuery({
-    queryKey: ["users"],
+  const [searchUser, setSearchUser] = useState()
+  const { refetch, data: users = [] } = useQuery({
+    queryKey: ["users", searchUser],
     queryFn: async () => {
-      const res = await axiosSecure.get(`/users`);
+      const res = await axiosSecure.get(`/users?searchUser=${searchUser}`);
       return res.data;
-    }
+    },
   });
 
-  const handleMakeAdmin = user =>{
-        Swal.fire({
-              title: "Are you sure?",
-              text: "You won't be able to revert this!",
-              icon: "warning",
-              showCancelButton: true,
-              confirmButtonColor: "#3085d6",
-              cancelButtonColor: "#d33",
-              confirmButtonText: "Yes, make his/her admin",
-            }).then(() => {
-              const roleInfo = { role: "admin"}
-        axiosSecure.patch(`/users/${user._id}/role`, roleInfo)
-        .then(res=>{
-            if(res.data.modifiedCount){
-                refetch()
-                Swal.fire({
-                          position: "top-end",
-                          icon: "success",
-                          title:
-                            `${user.displayName} marked as an admin`,
-                          showConfirmButton: false,
-                          timer: 2500,
-                        });
-            }
-        })
-            });
-    }
+  const handleMakeAdmin = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, make his/her admin",
+    }).then(() => {
+      const roleInfo = { role: "admin" };
+      axiosSecure.patch(`/users/${user._id}/role`, roleInfo).then((res) => {
+        if (res.data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.displayName} marked as an admin`,
+            showConfirmButton: false,
+            timer: 2500,
+          });
+        }
+      });
+    });
+  };
 
-    const handleRemoveAdmin = user =>{
-        Swal.fire({
-              title: "Are you sure?",
-              text: "You won't be able to revert this!",
-              icon: "warning",
-              showCancelButton: true,
-              confirmButtonColor: "#3085d6",
-              cancelButtonColor: "#d33",
-              confirmButtonText: "Yes, remove him/her!",
-            }).then(() => {
-              const roleInfo = { role: "user"}
-        axiosSecure.patch(`/users/${user._id}/role`, roleInfo)
-        .then(res=>{
-            if(res.data.modifiedCount){
-                refetch()
-                Swal.fire({
-                          position: "top-end",
-                          icon: "success",
-                          title:
-                            `${user.displayName} removed from admin`,
-                          showConfirmButton: false,
-                          timer: 2500,
-                        });
-            }
-        })
-            });
-    }
+  const handleRemoveAdmin = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, remove him/her!",
+    }).then(() => {
+      const roleInfo = { role: "user" };
+      axiosSecure.patch(`/users/${user._id}/role`, roleInfo).then((res) => {
+        if (res.data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.displayName} removed from admin`,
+            showConfirmButton: false,
+            timer: 2500,
+          });
+        }
+      });
+    });
+  };
 
   return (
     <div>
       <h2>Users: {users.length}</h2>
+      <label className="input">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+        <input
+          onChange={(e)=> setSearchUser(e.target.value)}
+          type="search"
+          required
+          placeholder="Search User" />
+      </label>
       <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
           <thead>
             <tr>
-              <th>
-                #
-              </th>
+              <th>#</th>
               <th>User</th>
               <th>Email</th>
               <th>Role</th>
@@ -88,44 +107,43 @@ const UserManager = () => {
             </tr>
           </thead>
           <tbody>
-            {users.map((user, index)=>
-                <tr>
-              <td>
-                {index +1}
-              </td>
-              <td>
-                <div className="flex items-center gap-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle h-12 w-12">
-                      <img
-                        src={user.photoURL}
-                        alt="Avatar"
-                      />
+            {users.map((user, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img src={user.photoURL} alt="Avatar" />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{user.displayName}</div>
+                      <div className="text-sm opacity-50">United States</div>
                     </div>
                   </div>
-                  <div>
-                    <div className="font-bold">{user.displayName}</div>
-                    <div className="text-sm opacity-50">United States</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                {user.email}
-              </td>
-              <td>{user.role}</td>
-              <th>
-                {user.role === "admin" ? 
-                <button 
-                onClick={()=> handleRemoveAdmin(user)}
-                className="btn"><GiShieldDisabled /></button> : 
-                <button
-                 onClick={()=> handleMakeAdmin(user)}
-                 className="btn"><FaUserShield /></button>
-                }
-              </th>
-            </tr>
-            )}
-            
+                </td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+                <th>
+                  {user.role === "admin" ? (
+                    <button
+                      onClick={() => handleRemoveAdmin(user)}
+                      className="btn"
+                    >
+                      <GiShieldDisabled />
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      className="btn"
+                    >
+                      <FaUserShield />
+                    </button>
+                  )}
+                </th>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
