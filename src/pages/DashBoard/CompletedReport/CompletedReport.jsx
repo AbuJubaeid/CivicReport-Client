22a1c8fc3bed38a898f9fc53d6e04d100438cf9a
AbuJubaeid <abujubaeidpranto2@gmail.com>
2026-02-10 01:00:12 +0600

    getting solved issue list

--- src/pages/DashBoard/CompletedReport/CompletedReport.jsx
+++ src/pages/DashBoard/CompletedReport/CompletedReport.jsx
@@ -1,8 +1,45 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useHook from "../../../hooks/useHook";
 
 const CompletedReport = () => {
+    const axiosSecure = useAxiosSecure();
+      const { user } = useHook();
+      const { data: reports = [], } = useQuery({
+        queryKey: ["reports", user.email, "In-Progress"],
+        queryFn: async () => {
+          const res = await axiosSecure.get(`/reports/staff?staffEmail=${user.email}&reportStatus=Solved`);
+          return res.data;
+        },
+      });
     return (
         <div>
-            <h2>Completed Report:</h2>
+            <h2>Completed Report:{reports.length}</h2>
+            <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Location</th>
+              <th>Report Status</th>
+              <th>Solved By</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reports.map((report, index) => (
+              <tr key={report._id}>
+                <th>{index + 1}</th>
+                <td>{report.issue}</td>
+                <td>{report.location}</td>
+                <td>{report.reportStatus}</td>
+                <td>{report.staffName}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
         </div>
     );
 };
